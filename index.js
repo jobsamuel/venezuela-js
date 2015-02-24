d6535385805cc95c59b0175bdef182e50b9d7b33
Jobsamuel Núñez <jobsamuel.nunez@outlook.com>
2015-02-23 23:04:21 -0430

    Se agregó el método 'estado'

--- index.js
+++ index.js
@@ -15,8 +15,31 @@ function Venezuela() {
 			return sub_array[sub_selector];
 		});
 	}
+	// Coloca en mayúscula la primera letra de cada palabra.
+	// (http://stackoverflow.com/a/196991/3366109)
+	function formato(texto)
+	{	
+	    return texto.replace(/\w\S*/g, function(txt) {
+	    	return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+	    });
+	}
 	this.pais = venezuela;
 	this.estados = mapa();
+	this.estado = function(nombre) {
+		venezuela.filter(function(entidad) {
+			var _nombre = formato(nombre);
+			if (entidad.estado == _nombre) {
+				var respuesta = {
+					iso_31662: entidad.iso_31662,
+					id_estado: entidad.id_estado,
+					estado: entidad.estado,
+					capital: entidad.capital,
+					municipios: entidad.municipios.length
+				}
+				return console.log(respuesta);
+			}
+		})
+	}
 	this.municipios = mapa(lista, "municipios", "municipio");
 	this.parroquias = mapa(lista, "municipios", "parroquias");
 }
