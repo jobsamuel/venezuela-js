445635937863fdd36fe0b327e36927430f183655
Jobsamuel Núñez <jobsamuel.nunez@outlook.com>
2015-02-24 23:46:16 -0430

    Se agregaron comentarios

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
 var venezuela = require('./venezuela.json');
 
 function Venezuela() {
+
+	// Retorna información sobre las entidades que conforman
+	// el territorio Venezuelano.
 	function mapa(callback, argumento, sub_argumento){
 		return venezuela.map(function (estado) {
 			if (callback) {
 				return callback(estado, argumento, sub_argumento);	
 			} else {
+
+				// Crea una lista de estados.
 				return estado.estado;
 			}
 		});
 	}
+
+	// Crea una lista de municipios o parroquias correspondientes a un estado.
 	function lista(array, selector, sub_selector) {
 		return array[selector].map(function (sub_array) {
 			return sub_array[sub_selector];
 		});
 	}
+	
 	// Coloca en mayúscula la primera letra de cada palabra.
 	// (http://stackoverflow.com/a/196991/3366109)
 	function formato(texto)
@@ -26,6 +34,8 @@ function Venezuela() {
 	this.pais = venezuela;
 	this.estados = mapa();
 	this.estado = function(nombre) {
+
+		// Retorna información sobre una entidad territorial específica.
 		venezuela.filter(function(entidad) {
 			var _nombre = formato(nombre);
 			if (entidad.estado == _nombre) {
