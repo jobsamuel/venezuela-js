c98111328857bfac24ac73e5cf7af86a41f8aabb
Jobsamuel Núñez <jobsamuel.nunez@outlook.com>
2018-01-04 11:58:05 -0400

    refactor(cli): reescribe condicional principal

--- cli.js
+++ cli.js
@@ -9,8 +9,6 @@ const amarillo = colors.yellow.bold('•');
 const azul = colors.blue.bold('•');
 const rojo = colors.red.bold('•');
 const bandera = `${amarillo} ${azul} ${rojo}`;
-let consulta;
-let respuesta;
 
 updateNotifier({pkg: pkg}).notify();
 
@@ -41,65 +39,29 @@ program.on('--help', () => {
   console.log(mensaje);
 });
 
-consulta = program.args ? program.args[0] : 'n/a';
+// TODO: Mostrar resultado de una consulta.
 
-if ((!consulta && program.capital) || (consulta && consulta.toLowerCase() === 'caracas')) {
-  respuesta = JSON.stringify(venezuela.capital, null, 4);
-} else if (program.ayuda || process.argv.length <= 2) {
-  return program.help();
-} else if (program.capital) {
-  if (venezuela.estado(consulta).capital) {
-    respuesta = `    ${venezuela.estado(program.args[0]).capital}`;
-  } else if (venezuela.municipio(consulta).capital) {
-    respuesta = `    ${venezuela.municipio(consulta).capital}`;
-  } else if (venezuela.parroquia(consulta).capital) {
-    respuesta = `    ${venezuela.parroquia(consulta).capital}`;
-  } else {
-    respuesta = `    ${consulta} es un nombre inválido.`;
-  }
-} else if (program.municipio) {
-  respuesta = JSON.stringify(venezuela.municipio(consulta), null, 4);
-} else if (program.parroquia) {
-  respuesta = JSON.stringify(venezuela.parroquia(consulta), null, 4);
-} else if (program.municipios && process.argv.length > 3) {
-  if (typeof venezuela.estado(consulta) === 'object') {
-    const datos = venezuela.estado(consulta, { municipios: true });
-    const estado = datos.estado.toUpperCase();
-    const titulo = colors.white.bold(`MUNICIPIOS DEL ESTADO ${estado}`);
+function consulta() {
+  const consulta = program.args ? program.args[0] : undefined;
 
-    respuesta = `\n  ${titulo}\n\n  ${parrafo(datos.municipios)}`;
+  if (!consulta && program.capital) {
+    return venezuela.capital;
+  } else if (program.capital) {
+    return (venezuela.estado(consulta).capital ||
+      venezuela.municipio(consulta).capital ||
+        venezuela.parroquia(consulta).capital);
+  } else if (program.municipios) {
+    return venezuela.estado(consulta, {municipios: true});
+  } else if (program.municipio) {
+    return venezuela.municipio(consulta);
+  } else if (program.parroquia) {
+    return venezuela.parroquia(consulta);
+  } else if (program.ayuda || process.argv.length <= 2) {
+    return 'n/a';
   } else {
-    respuesta = `    ${consulta} es un nombre inválido.`;
+    return consulta;
   }
-} else if (consulta) {
-  if (typeof venezuela.estado(consulta) === 'object') {
-    const datos = venezuela.estado(consulta)
-    const subtitulo1 = colors.yellow.bold(datos.iso_31662);
-    const subtitulo2 = colors.yellow.bold(datos.estado);
-    const subtitulo3 = colors.yellow.bold(datos.capital);
-    const subtitulo4 = colors.yellow.bold(datos.municipios);
-    const subtitulo5 = colors.yellow.bold(datos.parroquias);
-    const titulo1 = colors.white.bold(`   ISO 31662    ${subtitulo1}`);
-    const titulo2 = colors.white.bold(`      ESTADO    ${subtitulo2}`);
-    const titulo3 = colors.white.bold(`     CAPITAL    ${subtitulo3}`);
-    const titulo4 = colors.white.bold(`  MUNICIPIOS    ${subtitulo4}`);
-    const titulo5 = colors.white.bold(`  PARROQUIAS    ${subtitulo5}`);
-
-    respuesta = `${titulo1}\n${titulo2}\n${titulo3}\n${titulo4}\n${titulo5}`
-  } else if (typeof venezuela.municipio(consulta) === 'object') {
-    respuesta = JSON.stringify(venezuela.municipio(consulta), null, 4);
-  } else if (typeof venezuela.parroquia(consulta) === 'object') {
-    respuesta = JSON.stringify(venezuela.parroquia(consulta), null, 4);
-  } else {
-    respuesta = `    ${consulta} es un nombre inválido.`;
-  }
-} else {
-  respuesta = '    Debes ingresar un nombre.';
 }
-
-const mensaje = `  ${bandera}\n${respuesta}`;
-
-console.log(mensaje);
 
 function parrafo (palabras) {
   const separador = colors.grey.bold('•');
